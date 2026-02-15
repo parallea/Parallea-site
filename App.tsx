0559486c4a913f9bd30076fb5c906f7f3da1252f
parallea <parallea01@gmail.com>
2026-02-15 15:58:35 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -52,25 +52,32 @@ const App: React.FC = () => {
         <TextReveal />
         <VerticalBookSlider />
 
-        <section className="team">
-          <div className="container">
-            <div className="team-grid">
-                <div className="team-member">
-                    <div className="team-photo founder" style={{ backgroundImage: 'url(/images/profiles/ayush.jpg)', backgroundSize: 'cover', backgroundPosition: 'center' }}></div>
-                    <h3>Ayush Kumar<br />Bhardwaj</h3>
-                    <p>Founder</p>
-                    <a href="https://www.linkedin.com/in/ayushkumarbhardwaj" className="linkedin-link"></a>
-                </div>
-                <div className="team-member">
-                    <div className="team-photo director" style={{ backgroundImage: 'url(/images/profiles/manish.jpg)', backgroundSize: 'cover', backgroundPosition: 'center' }}></div>
-                    <h3>Manish<br />Kumar</h3>
-                    <p>Director, Head of Design</p>
-                    <a href="https://www.linkedin.com/in/manishkr369" className="linkedin-link"></a>
-                </div>
-            </div>
-          </div>
-        </section>
-
+<section className="team">
+  <div className="container">
+    <div className="team-grid">
+        <div className="team-member">
+            <div className="team-photo founder" style={{ backgroundImage: 'url(/images/profiles/ayush.jpg)', backgroundSize: 'cover', backgroundPosition: 'center' }}></div>
+            <h3>Ayush Kumar<br />Bhardwaj</h3>
+            <p>Founder</p>
+            <a href="https://www.linkedin.com/in/ayushkumarbhardwaj" className="linkedin-link" target="_blank" rel="noopener noreferrer">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="white">
+                <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"/>
+              </svg>
+            </a>
+        </div>
+        <div className="team-member">
+            <div className="team-photo director" style={{ backgroundImage: 'url(/images/profiles/manish.jpg)', backgroundSize: 'cover', backgroundPosition: 'center' }}></div>
+            <h3>Manish<br />Kumar</h3>
+            <p>Director, Head of Design</p>
+            <a href="https://www.linkedin.com/in/manishkr369" className="linkedin-link" target="_blank" rel="noopener noreferrer">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="white">
+                <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"/>
+              </svg>
+            </a>
+        </div>
+    </div>
+  </div>
+</section>
         <footer className="py-20 md:py-32 flex flex-col items-center bg-black border-t border-white/20 px-4 relative overflow-hidden">
           {/* Footer Glow - White */}
           <div className="absolute top-0 left-1/2 -translate-x-1/2 w-full max-w-lg h-1 bg-white blur-[100px] opacity-40"></div>
